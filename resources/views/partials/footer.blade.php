e024575fa883aad507122b3c104b872dc907c228
lelieriyani04 <lelieriyanihasanah@gmail.com>
2025-01-14 12:54:00 +0700

    Delete resources directory

--- resources/views/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="py-4 bg-light mt-auto">
-    <div class="container-fluid px-4">
-        <div class="d-flex align-items-center justify-content-center small">
-            Copyright &copy; Laracoffee
-            <?= date('Y'); ?>
-        </div>
-    </div>
-</footer>
