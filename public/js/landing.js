13092474e807c801b16b3aa17a02425173ea6639
lelieriyani04 <lelieriyanihasanah@gmail.com>
2025-01-14 12:53:46 +0700

    Delete public directory

--- public/js/landing.js
+++
@@ -1,6 +0,0 @@
-$(".masthead").mousemove(function (event) {
-    var mouseX = (event.pageX * -1) / 150;
-    var mouseY = (event.pageY * -1) / 150;
-
-    $(this).css("background-position", mouseX, mouseY);
-});
