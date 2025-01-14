bc5d6d3961150862628bb00afdbd0a5c97ae487e
lelieriyani04 <lelieriyanihasanah@gmail.com>
2025-01-14 13:27:15 +0700

    first commit

---
+++ resources/views/partials/home/home_customers.blade.php
@@ -0,0 +1,73 @@
+<section id="hero">
+  <div class="container mt-0 mt-lg-5">
+    <div class="row">
+      <div class="col-md-6 pt-5 pt-lg-0 order-md-1 d-flex flex-column justify-content-center" data-aos="fade-up">
+        <div>
+          <h1>New way to enjoy quality coffee</h1>
+          <p>Temukan pengalaman menikmati kopi yang berbeda, di mana kualitas bertemu dengan kenyamanan. Dari biji kopi pilihan hingga proses penyajian yang sempurna, kami hadir untuk membawa secangkir kebahagiaan ke dalam hari Anda.</p>
+          <a href="#about" class="btn-get-started scrollto">Get Started</a>
+        </div>
+      </div>
+      <div class="col-md-6 order-md-2 mt-5 mt-lg-0 pt-5 pt-lg-0 d-none d-md-block hero-img" data-aos="fade-left">
+        <img src="{{ asset('storage/home/coffee.jpg') }}" class="img-fluid" alt="Coffee">
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<main id="main">
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+
+    <div class="container" data-aos="fade-up">
+      <div class="row">
+
+        <div class="col-lg-5 col-md-6 d-none d-md-block">
+          <div class="about-img" data-aos="fade-right" data-aos-delay="100">
+            <img src="{{ asset('storage/home/laracoffee.jpg') }}" alt="laracoffee">
+          </div>
+        </div>
+
+        <div class="col-lg-7 col-md-6">
+          <div class="about-content" data-aos="fade-left" data-aos-delay="100">
+            <h2>About Laracoffee</h2>
+            <p> sebuah kedai kopi yang mengusung konsep modern dengan sentuhan kehangatan tradisional, dirancang untuk menjadi tempat favorit pecinta kopi sejati. Dengan komitmen menghadirkan pengalaman terbaik, LaraCoffee menggunakan biji kopi pilihan yang diolah secara profesional untuk menghasilkan rasa yang kaya dan autentik. Selain kopi, LaraCoffee juga menawarkan berbagai pilihan makanan ringan dan menu minuman lainnya, menciptakan suasana yang sempurna untuk berkumpul, bekerja, atau sekadar bersantai. Dengan desain interior yang nyaman dan pelayanan ramah, LaraCoffee adalah tempat di mana setiap cangkir kopi menyimpan cerita dan setiap kunjungan menjadi momen istimewa.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section id="services" class="services section-bg">
+    <div class="container">
+
+      <div class="section-title" data-aos="fade-up">
+        <h2>Additional Information</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12 col-lg-6 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in">
+          <div class="icon-box icon-box-pink">
+            <div class="icon"><i class="fa fa-fw fa-dumpster"
+                style="font-size: 48px;margin-bottom: 15px;line-height: 1;color:orange;"></i></div>
+            <h4 class="title"><a href="">Why Laracoffee?</a></h4>
+            <p class="description">Lara Coffee adalah lebih dari sekadar kedai kopi; ini adalah tempat di mana kualitas, rasa, dan kenyamanan berpadu untuk menciptakan pengalaman yang tak terlupakan. Dengan biji kopi terbaik yang diproses secara hati-hati, setiap cangkir yang kami sajikan membawa kelezatan autentik yang memanjakan lidah. Ditambah suasana hangat dan desain interior yang modern, LaraCoffee menjadi pilihan sempurna untuk bersantai, bertemu teman, atau menyelesaikan pekerjaan. Kami juga menawarkan beragam pilihan menu yang dirancang untuk memenuhi berbagai selera, menjadikan LaraCoffee destinasi ideal bagi pecinta kopi dan kehangatan.</p>
+            <p class="description">Pilih LaraCoffee untuk rasa yang istimewa dan momen yang berkesan.</p>
+          </div>
+        </div>
+
+        <div class="col-md-12 col-lg-6 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="100">
+          <div class="icon-box icon-box-cyan">
+            <div class="icon"><i class="fa fa-basket-shopping"
+                style="font-size: 48px;margin-bottom: 15px;line-height: 1;color:#3fcdc7;"></i></div>
+            <h4 class="title"><a href="">Easy way to order!</a></h4>
+            <p class="description">Lakukan pemesanan melalui website ini dan dapatkan diskonnya!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
